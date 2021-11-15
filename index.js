2fbfa09700088f65f527a365e915189ce360e0d3
Ulugbek Nuriddinov <42731921+ucoder92@users.noreply.github.com>
2021-11-15 19:54:31 +0300

    Version 1.0.1

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ module.exports = class Fkit {
             publicPath: path.resolve(mainPath, 'public'),
             pagesPath: path.resolve(mainPath, 'src/pages'),
             layoutsPath: path.resolve(mainPath, 'src/layouts'),
-            sectionsPath: path.resolve(mainPath, 'src/sections'),
         }
     }
 
@@ -32,7 +31,6 @@ module.exports = class Fkit {
         const pagesPath = this.config.pagesPath;
         const publicPath = this.config.publicPath;
         const layoutsPath = this.config.layoutsPath;
-        const sectionsPath = this.config.sectionsPath;
 
         // Check pages path
         if (!fs.existsSync(pagesPath)) {
@@ -42,11 +40,6 @@ module.exports = class Fkit {
         // Check layouts path
         if (!fs.existsSync(layoutsPath)) {
             fs.mkdirSync(layoutsPath);
-        }
-
-        // Check sections path
-        if (!fs.existsSync(sectionsPath)) {
-            fs.mkdirSync(sectionsPath);
         }
 
         // Check public path
